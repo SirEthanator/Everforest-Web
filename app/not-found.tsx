3aeaf30b836b319151d8f4b848d87d261c1548ec
Ethan <e.slimerancher@gmail.com>
2024-10-27 12:37:15 +1300

    404: Add footer

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "@/styles/NotFound.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function NotFound() {
   return (
@@ -10,6 +11,7 @@ export default function NotFound() {
       <h1 className="errCode">404</h1>
       <h1 className="notFoundTitle">Page not found</h1>
     </div>
+    <Footer />
   </body>
   )
 }
