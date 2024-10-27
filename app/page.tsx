8cf3d50bc6acbc3b9d604708f1ab4e77f7b31b4b
Ethan <e.slimerancher@gmail.com>
2024-10-28 12:48:47 +1300

    Add homepage image

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import '@/styles/home.css'
@@ -13,7 +14,12 @@ export default function Page() {
           <h1 className='homeTitle'>Warm, comfortable, pleasant</h1>
           <h2 className='homeDesc'>Everforest is a green based color scheme; it's designed to be warm and soft in order to protect developers' eyes.</h2>
         </div>
-        {/* Add theme preview here */}
+        <div className='homeImageWrapper'>
+          <Image src="https://user-images.githubusercontent.com/37491630/206063892-e1b2197e-2404-4ab4-8570-3dee96242ba1.png"
+                 alt="Screenshot of Everforest Vim"
+                 fill
+                 className='homeImage'/>
+        </div>
       </div>
 
       <Footer />
