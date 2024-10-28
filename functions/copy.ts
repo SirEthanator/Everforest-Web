7eabc394873174346d9ac2777cbbb872f7ed5414
Ethan <e.slimerancher@gmail.com>
2024-10-28 19:04:50 +1300

    Copy function: simplify some code

--- functions/copy.ts
+++ functions/copy.ts
@@ -3,27 +3,27 @@
 import { getColours, getColoursL } from "@/functions/colours";
 
 export function copy(colourID:string, theme:string='dark'): void {
-  let colours:Array<Array<string>>
+  let colours:Array<Array<string>>;
   if (theme === 'light') { colours = getColoursL() }
   else { colours = getColours() };
 
   let rowIndex = colours.findIndex(row => row.indexOf(colourID) !== -1);
-  let element = document.getElementById(colourID)
-  let colour:string
-  let newText:string
+  let element = document.getElementById(colourID);
+  let colour = 'green';
+  let newText = 'Copied!';
   try {
-    colour = 'green'
-    newText = 'Copied!'
     navigator.clipboard.writeText(colours[rowIndex][1]);
   } catch {
-    colour = 'red'
-    newText = 'Failed!'
+    colour = 'red';
+    newText = 'Failed!';
   };
 
-  element.style.color = 'var(--'+colour+')';
-  setTimeout(() => {element.style.color = 'var(--fg)'}, 2000);
   let oldText = element.innerHTML;
-  element.innerHTML = `<i class="nf nf-fa-copy"></i> ${newText}`
-  setTimeout(() => {element.innerHTML = oldText}, 2000);
+  element.style.color = 'var(--'+colour+')';
+  element.innerHTML = `<i class="nf nf-fa-copy"></i> ${newText}`;
+  setTimeout(() => {
+    element.style.color = 'var(--fg)';
+    element.innerHTML = oldText;
+  }, 2000);
 }
 
