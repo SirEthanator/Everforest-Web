f8846c126e13fc658858f68dfa5ed153d93fea8a
Ethan <e.slimerancher@gmail.com>
2024-10-28 13:53:24 +1300

    Footer: Change capitalisation on link because I can

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <div className="footer">
       <div className="footerContent">
-        <a href='https://github.com/sirethanator/everforest-web' target="_blank">
+        <a href='https://github.com/SirEthanator/Everforest-Web' target="_blank">
           <Image src={githubLogo} width={32} height={32}
         alt="Github logo" />
         </a>
