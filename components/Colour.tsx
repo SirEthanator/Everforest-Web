8d438595b6f73f7240ef775755dc767cb6f17280
Ethan <e.slimerancher@gmail.com>
2024-10-31 14:58:25 +1300

    Colour: Add bottom margin to last item

--- components/Colour.tsx
+++ components/Colour.tsx
@@ -6,7 +6,7 @@ import {copy} from '@/functions/copy';
 export default function Colour({ colourID, hex, name, theme }) {
   let style = {}
   if (colourID.includes('gray2')) {
-    style = {border: 'none'}
+    style = {border: 'none', marginBottom: '10px'}
   }
   return (
     <div className='color' style={style}>
