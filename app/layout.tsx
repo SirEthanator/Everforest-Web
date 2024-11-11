0c1adb148c8b59c9eaeb043fb89491942dd034b6
Ethan <e.slimerancher@gmail.com>
2024-11-11 18:03:31 +1300

    Add verification for Google Search Console

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,8 @@ export const metadata: Metadata = {
   title: "Everforest",  // Default title if not specified on page
   description: "A warm, comfortable, and pleasant color scheme based around green designed to be easy on the eyes.",
   keywords: ['everforest', 'green', 'colorscheme', 'colourscheme', 'color scheme', 'colour scheme', 'color palette', 'colour palette'],
-  authors: [{name: 'Ethan M.', url: 'https://github.com/SirEthanator'}]
+  authors: [{name: 'Ethan M.', url: 'https://github.com/SirEthanator'}],
+  verification: { google: "FjIB76Sma1-os92AEkFdtb5Hl5Sf20bXW8BfH2WzGVQ" }
 }
 
 export default function RootLayout({
