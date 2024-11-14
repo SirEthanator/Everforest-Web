e9c1531e3e6608f95007d3a5317d03940c2786cc
Ethan <e.slimerancher@gmail.com>
2024-11-14 17:23:27 +1300

    NextJS: Silence SASS legacy js API warnings

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,13 @@
-module.exports = {
+import { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  sassOptions: {
+    silenceDeprecations: ['legacy-js-api'],
+  },
   images: {
     remotePatterns: [{ protocol: "https", hostname: '**'}]
   }
 }
+
+module.exports = nextConfig
+
